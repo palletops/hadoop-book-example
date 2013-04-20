7b42e8ea10fa4cd4e23c35a9711d775b1c3578a6
Hugo Duncan <hugo@hugoduncan.org>
2013-04-20 14:22:13 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/hadoop-book-example "0.1.0"
+(defproject com.palletops/hadoop-book-example "0.1.1-SNAPSHOT"
   :description "Hadoop book example"
   :url "https://github.com/palletops/hadoop-book-example"
   :license {:name "All rights reserved."}
