6f2deef8c1cf40ee2b1d4688810803d2afff7c9a
Hugo Duncan <hugo@hugoduncan.org>
2013-04-20 13:52:17 -0400

    Update for the latest pallet beta

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:dev
+ {:dependencies [[org.cloudhoist/pallet-vmfest "0.2.1"]
+                 [com.palletops/pallet "0.8.0-SNAPSHOT"
+                  :classifier "tests"]
+                 [com.palletops/hadoop-crate "0.1.4-SNAPSHOT"]
+                 [ch.qos.logback/logback-classic "1.0.0"]]}}
--- project.clj
+++ project.clj
@@ -3,13 +3,4 @@
   :url "https://github.com/palletops/hadoop-book-example"
   :license {:name "All rights reserved."}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.cloudhoist/pallet "0.8.0-SNAPSHOT"]]
-  :profiles {:dev
-             {:dependencies [[org.cloudhoist/pallet-vmfest "0.2.1"]
-                             [org.cloudhoist/pallet "0.8.0-SNAPSHOT"
-                              :classifier "tests"]
-                             [com.palletops/hadoop-crate "0.1.4-SNAPSHOT"]
-                             [ch.qos.logback/logback-classic "1.0.0"]]}}
-  :repositories
-  {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
-   "sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
+                 [com.palletops/pallet "0.8.0-SNAPSHOT"]])
